b750d4c1a0ceb07e2f8139c997f37b84684228c6
anchabadze <zurab.anchabadze@gmail.com>
2024-02-08 18:57:57 -0300

    Added two new tests

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription, FundSubsciption, AddConsumer} from "script/Interactions.s.sol";
+
+contract DeployRaffle is Script {
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit,
+            address link
+        ) = helperConfig.activeNetworkConfig();
+
+        if (subscriptionId == 0) {
+            // if we don't have a subscription
+            // we are going to need to create a subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            subscriptionId = createSubscription.createSubscription(
+                vrfCoordinator
+            );
+            // then we are going to need to fund the subscription
+            FundSubsciption fundSubsciption = new FundSubsciption();
+            fundSubsciption.fundSubsciption(
+                vrfCoordinator,
+                subscriptionId,
+                link
+            );
+        }
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            vrfCoordinator,
+            subscriptionId
+        );
+        return (raffle, helperConfig);
+    }
+}
---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract RaffleTest is Test {
+    /* Events */
+
+    event EnteredRaffle(address indexed player);
+
+    Raffle raffle;
+    HelperConfig helperConfig;
+
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint64 subscriptionId;
+    uint32 callbackGasLimit;
+    address link;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.run();
+        (
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit,
+            link
+        ) = helperConfig.activeNetworkConfig();
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    //////////////////
+    // enterRaffle  //
+    //////////////////
+
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        // Arrange
+        vm.prank(PLAYER); // pretending to be the player
+        // Act / Assert
+        // expecting to be revert, because doesn't have enough ETH
+        vm.expectRevert(Raffle.Raffle__NotEnoughEthSent.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        assert(playerRecorded == PLAYER);
+    }
+
+    function testEmitsEventOnEntrance() public {
+        vm.prank(PLAYER);
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit EnteredRaffle(PLAYER); // emit the event that we expecting
+        raffle.enterRaffle{value: entranceFee}(); // function call that should emit this event
+    }
+
+    function testCantEnterWhenRaffleIsCalculating() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep(""); // after we done it raffle should be in calculating state
+        vm.expectRevert(Raffle.RaffleNotOpen.selector);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+    }
+
+    //////////////////
+    // checkUpkeep  //
+    //////////////////
+
+    function testCheckUpkeekReturnsFalseIfIthasNoBalance() public {
+        // Arrange
+        vm.warp(block.timestamp + interval + 1); // make sure that enough time has passed
+        vm.roll(block.number + 1); // make sure that enough blocks are confirmed
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+        // Assert
+        assert(!upkeepNeeded); // assert not false (which is true)
+    }
+
+    function testCheckUpkeepReturnsFalseIfRaffleNotOpen() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1); // make sure that enough time has passed
+        vm.roll(block.number + 1); // make sure that enough blocks are confirmed
+        raffle.performUpkeep(""); // after we done it raffle should be in calculating state
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+        // Assert
+        assert(upkeepNeeded == false);
+    }
+
+    // testCheckUpkeepReturnsFalseIfEnoughTimeHasntPassed
+    // testCheckUpkeepReturnsTrueWhenParametersAreGood
+}
